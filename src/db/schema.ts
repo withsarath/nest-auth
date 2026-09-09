a1e61e4853b1628ca69f0db4126c2d40704a62c1
withsarath <withsarath.m@gmail.com>
2026-09-09 22:21:05 +0530

    feat(database): Added database connection with drizzle-orm and neon

---
+++ src/db/schema.ts
@@ -0,0 +1,50 @@
+import {
+  pgTable,
+  text,
+  pgEnum,
+  timestamp,
+  uuid,
+  boolean,
+} from 'drizzle-orm/pg-core';
+
+export const userRoleEnum = pgEnum('user_role', ['user', 'admin']);
+
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: text('email').notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+  name: text('name').notNull(),
+  role: userRoleEnum('role').notNull().default('user'),
+  isVerified: boolean('is_verified').notNull().default(false),
+  verificationToken: text('verification_token'),
+  verificationTokenExpiresAt: timestamp('verification_token_expires_at'),
+  resetToken: text('reset_token'),
+  resetTokenExpiresAt: timestamp('reset_token_expires_at'),
+  refreshTokenHash: text('refresh_token_hash'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const taskStatusEnum = pgEnum('task_status', [
+  'todo',
+  'in_progress',
+  'done',
+]);
+
+export const tasks = pgTable('tasks', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  title: text('title').notNull(),
+  description: text('description'),
+  status: taskStatusEnum('status').notNull().default('todo'),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+// Drizzle can generate the TypeScript type for us. insted of we manually create types
+export type User = typeof users.$inferSelect; //give me types of select from the database
+export type NewUser = typeof users.$inferInsert; //give me types of insert into the database
+export type Task = typeof tasks.$inferSelect;
+export type NewTask = typeof tasks.$inferInsert;
